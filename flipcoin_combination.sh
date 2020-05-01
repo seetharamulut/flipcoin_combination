8b447480427ec5dca5cceec8f88c3cddce839454
seetharamulu <thumurugoti.seetharamulu@gmail.com>
2020-05-01 14:41:07 +0530

    added flipcoin_combination.sh

--- flipcoin_combination.sh
+++ flipcoin_combination.sh
@@ -1,8 +1,17 @@
-#! /bin/bash/
-random=$(( $RANDOM%2 ))
-if [ $random -eq 1 ]
-then
-   echo "heads won"
-else
-   echo "tails won"
-fi
+#! /bin/bash/ 
+declare -A count=( [heads]="0" [tails]="0" )
+total_count=0
+
+while [ $total_count -lt 50 ]
+do
+  total_count=$(( $total_count+1))
+  random=$(( $RANDOM%2 ))
+  if [ $random -eq 1 ] 
+  then
+   count[heads]=$(( ${count[heads]}+1 ))
+  else
+   count[tails]=$(( ${count[tails]}+1 ))
+  fi
+done
+echo "heads percentage in singlet combination is :" $(( 100*${count[heads]}/$total_count ))
+echo "tails percentage is singlet combination is :" $(( 100*${count[tails]}/$total_count ))
